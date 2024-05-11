f80a39b51ad54cb806028bc88ea12a14053f9466
Drei4 <antipin-lukinf@mail.ru>
2024-05-11 18:10:15 +0300

    hw2 v1

---
+++ myHW/home_workapp/management/commands/add_client.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+
+from myHW.home_workapp.models import *
+# from User.models import *
+
+
+name = input('Введите имя клиента: ')
+email = input('Введите E-mail клиента: ')
+age = input('Введите возраст клиента: ')
+
+
+class Command(BaseCommand):
+    help = 'Add user'
+
+    def handle(self, *args, **kwargs):
+        user = User(name=name, email=email, age=age)
+        user.save()
+        self.stdout.write(f'{user}')
